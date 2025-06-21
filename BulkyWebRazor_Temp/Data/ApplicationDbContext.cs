7e921ba80cae13c5aff96567413faf1adb629b52
muhammad hamza khan <mh9813951@gmail.com>
2025-06-21 21:26:57 +0500

    Adding Razor Pages project

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Action", CategoryDisplayOrder = 1 },
+                new Category { CategoryId = 2, CategoryName = "SciFi", CategoryDisplayOrder = 2 },
+                new Category { CategoryId = 3, CategoryName = "History", CategoryDisplayOrder = 3 });
+        }
+    }
+}
---
+++ BulkyWebRazor_Temp/Pages/categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
