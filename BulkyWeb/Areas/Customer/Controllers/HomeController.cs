6873729cc156cfbf8eab18d7610ef6f0e2ab07f8
muhammad hamza khan <mh9813951@gmail.com>
2025-07-05 16:30:59 +0500

    populating product for customers

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using System.Diagnostics;
+using Bulky.DataAccess.Resository;
+using Bulky.DataAccess.Resository.IRepository;
+using Bulky.Models;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +11,21 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _unitOfWork.ProductRepository.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details()
         {
             return View();
         }
